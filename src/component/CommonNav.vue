dd2fe16877f5009f3f0de9c0846b1c1478e8c14a
PythonWongXb <739221432@qq.com>
2020-07-06 21:15:08 +0800

    nav完成

--- src/component/CommonNav.vue
+++ src/component/CommonNav.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-07-06 15:31:37
- * @LastEditTime: 2020-07-06 17:11:25
+ * @LastEditTime: 2020-07-06 21:13:26
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /realmimall/src/component/CommonNav.vue
@@ -9,19 +9,132 @@
 <template>
   <div class="commonNav">
     <div class="container">
-      555
+      <div class="left-part">
+        <a href="javascript:;">
+          <span>小米商城</span>
+        </a>
+        <a href="javascript:;">
+          <span>MIUI</span>
+        </a>
+        <a href="javascript:;">
+          <span>loT</span>
+        </a>
+        <a href="javascript:;">
+          <span>云服务</span>
+        </a>
+        <a href="javascript:;">
+          <span>金融</span>
+        </a>
+        <a href="javascript:;">
+          <span>有品</span>
+        </a>
+        <a href="javascript:;">
+          <span>小爱开放平台</span>
+        </a>
+        <a href="javascript:;">
+          <span>企业团购</span>
+        </a>
+        <a href="javascript:;">
+          <span>资质郑照</span>
+        </a>
+        <a href="javascript:;">
+          <span>协议规则</span>
+        </a>
+        <a href="javascript:;">
+          <span>下载app</span>
+        </a>
+        <a href="javascript:;">
+          <span>Select Location</span>
+        </a>
+      </div>
+      <div class="right-part">
+        <a v-if="!username" href="javascript:;">登录</a>
+        <user-index v-if="username"></user-index>
+        <a href="javascript:;">注册</a>
+        <a href="javascript:;">消息通知</a>
+        <a class="cart-box" href="javascript:;">
+          <div class="cart">
+            <i class='icon-circle-users'></i>
+            <span>购物车</span>
+            <span>({{ cartcount }})</span>
+          </div>
+          <div class='all-order'>
+            什么也没有
+          </div>
+          </a>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import UserIndex from '@/component/UserIndex'
+
 export default {
-  name: 'CommonNav'
+  name: 'CommonNav',
+  data () {
+    return {
+      // vuex
+      username: 'Ppythonista',
+      carcount: 0
+    }
+  },
+  components: {
+    UserIndex
+  }
 }
 </script>
 
 <style lang='sass' scoped>
-@import '../assets/scss/base.sass'
+@import '../assets/sass/mixin'
+@import '../assets/sass/base'
+@import '../assets/sass/config'
+
 .commonNav
+  background-color: $colorB
   .container
+    @include flex()
+    color: $colorL
+    height: 40px
+
+    a
+      margin-right: 8px
+      font-size: 12px
+      line-height: 40px
+      &:hover
+        color: $colorG
+
+    .left-part
+    .right-part
+      @include flex()
+      .cart-box
+        position: relative
+        display: block
+        margin: 0 0 0 22px
+        width: 120px
+        background: $colorM
+
+        &:hover
+          .all-order
+            height: 100px
+        .cart
+          @include flex()
+          justify-content: center
+
+          &:hover
+            color: $colorA
+            background: $colorG
+          i
+            margin-right: 5px
+        .all-order
+          color: $colorL
+          position: absolute
+          height: 0px
+          width:316px
+          right: 0
+          text-align: center
+          line-height: 100px
+          overflow: hidden
+          transition: height .5s
+          @include shadow()
 </style>
--- src/component/UserIndex.vue
+++ src/component/UserIndex.vue
@@ -1,13 +1,27 @@
 <!--
  * @Author: your name
  * @Date: 2020-07-06 15:38:38
- * @LastEditTime: 2020-07-06 16:31:40
+ * @LastEditTime: 2020-07-06 21:13:03
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /realmimall/src/component/UserIndex.vue
 -->
 <template>
-  <div>UserIndex</div>
+  <div class='userindex'>
+    <div class='top'>
+      <div class="name">Pythonista Wong</div>
+      <div class="icon iconfont"></div>
+    </div>
+    <div class="bottom">
+      <div class="bottom-box">
+        <div class="">个人中心</div>
+        <div class="">晒单评价</div>
+        <div class="">我的喜欢</div>
+        <div class="">小米账户</div>
+        <div class="">退出登录</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -17,5 +31,61 @@ export default {
 </script>
 
 <style lang='sass' scoped>
+@import '../assets/sass/mixin'
+@import '../assets/sass/base'
+@import '../assets/sass/config'
+.userindex
+  position: relative
+  width: 110px
+  height: 40px
+  margin-right: 16px
+  line-height: 40px
+  font-size: 12px
+  cursor: pointer
+  &:hover
+    background: $colorG
+    color: $colorA
+  &:hover
+    .bottom
+      height: 164px
+      box-shadow: 1px 3px 3px rgba(0, 0, 0, 0.15)
+      opacity: 1
+  .top
+    @include flex()
 
+    .name
+      padding: 0 5px
+      width: 75px
+      overflow: hidden
+      white-space: nowrap
+      text-overflow: ellipsis
+    .icon
+      flex: 1
+      text-align: center
+      &::after
+        content: '\e65b'
+        font-size: bold
+  .bottom
+    position: absolute
+    opacity: 0
+    color: $colorL
+    top: 100%
+    height: 0px
+    width: 110px
+    background: $colorG
+    transition: all .4s
+    box-shadow: 1px 0px 3px rgba(0, 0, 0, 0.15)
+    overflow: hidden
+    .bottom-box
+      padding: 7px 0
+      text-align: center
+      height: 164px
+      @include flexc()
+      div
+        height:30px
+        width: 100%
+        line-height: 30px
+        &:hover
+          color: $colorA
+          background-color:$colorN
 </style>
